ee7a6fae3552b9cda04591dcad56d7beecd8c386
Karan Sharma <karansharma1295@gmail.com>
2015-07-06 00:20:10 +0530

    added userschema

--- breeze/public/services/api.js
+++ breeze/public/services/api.js
@@ -4,5 +4,42 @@ var Schema       = mongoose.Schema;
 var EventSchema   = new Schema({
 	name: String
 });
+var userSchema = new mongoose.Schema({
+  name: { type: String, trim: true, required: true },
+  email: { type: String, unique: true, lowercase: true, trim: true },
+  password: String,
+  facebook: {
+    id: String,
+    email: String
+  },
+  google: {
+    id: String,
+    email: String
+  }
+});
+
+userSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) return next();
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+var User = mongoose.model('User', userSchema);
+var Show = mongoose.model('Show', showSchema);
+
 
 module.exports = mongoose.model('Event', EventSchema);
