d14a75eaaee3f270cd7c592c9c6b20fd808114d6
techalien <akhil@techalien.in>
2015-08-12 23:48:53 +0530

    dropdowns

--- breeze/public/controllers/form.js
+++ breeze/public/controllers/form.js
@@ -1,12 +1,50 @@
 angular.module('breeze')
   .controller('AddCtrl', function($scope, $alert, Event,$http,$location,$filter) {
     $scope.message_head = "Register for SNU Breeze 2015 Events here";
-
-    $scope.events = [
-      'event 1',
-      'event 2',
-      'event 3'
+    $scope.clubNames = [
+      'SNUPHORIA',
+      'INFERNO',
+      'WORDS.Ink',
+      'IMPRINTS',
+      'SIGREE',
+      'CINE U',
+      'ECONOMICS SOCIETY',
+      'Kalakriti',
+      'SNU ACM CHAPTER',
+      'ORION-SNUCES',
+      'FACTION',
+      'MUNSOC',
+      'RoboYantriki',
+      'The Mechanical Society',
+      'SNU-IEEE'
     ];
+
+    var eventlist = [
+      
+        ["Armonia: Solo Singing Competition","Crescendo: Battle of Bands","Acoustyx: Acoustic Band Competion"]
+      ,
+      
+        ["Singularity: Solo Dance","Twin Paradox: Dual Dance","Time Warpers: Group Dance"],
+        ['Treasure Hunt: Time Quest','Debate: War of Words','Creative Writing: Tempus Fugit'],
+        ['Aagaz','The Dumb Show','Dialoguebaazi'],
+        ['Pumperfection','Drink and Drive','Street Feast'],
+        ['Time Wreck'],
+        ['Casino Royale','This is Trading','Stock Market'],
+        ['Fashion Fiesta','Play with Clay','Filmistaan'],
+        ['Hackathon','Code Debugging','Coding Championship'],
+        ['Cantilivo','Intelligence: Civil Engineering Quiz'],
+        ['Retro Quiz','The X-Factor Quiz','Online Quiz'],
+        ['MUN 2050'],
+        ['Sands of Time','Robo-Combat','Future Drive'],
+        ['Assemble It','Design Smackdown','Robo-Combat'],
+        ['Analogue Maker','Wire-o-logy']
+    ];
+
+    $scope.onchange = function() {
+      var key = $scope.clubNames.indexOf($scope.clubName);
+      $scope.events = eventlist[key];
+    };
+    
 
     $http.get('/api/me')
     .success(function(data){
